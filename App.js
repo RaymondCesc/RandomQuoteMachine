f583c846989c66220a30cd6d7b3bdcef733f61a2
Edeamrere Raymond <50088982+RaymondCesc@users.noreply.github.com>
2020-12-11 10:53:56 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Mainbody from './Mainbody'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Mainbody />
+    </div>
+  );
+}
+
+export default App;
